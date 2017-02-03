a56853b199aac8a5bd5ea3247678c0441ad65a68
Eric C. Weig <eweig@email.uky.edu>
2017-02-03 14:02:52 -0500

    Create ia_kdnp_replace_publicdate.sh

---
+++ ia_kdnp_replace_publicdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+catalog=/home/path_to_public/public_html/papervault
+tla=$1
+ark=$2
+
+sed -i 's/"publicdate":"/"publicdate":"\n/' ${catalog}/${tla}/${ark}.pdt
+sed -i -e '1d' ${catalog}/${tla}/${ark}.pdt
+sed -i -e '2d' ${catalog}/${tla}/${ark}.pdt
+sed -i 's/"}//' ${catalog}/${tla}/${ark}.pdt
+sed -i 's/ //' ${catalog}/${tla}/${ark}.pdt
+sed -i 's/\n//' ${catalog}/${tla}/${ark}.pdt
